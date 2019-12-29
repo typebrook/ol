1ee17e46a7ad6717d52147e8eebb1544a940b77d
typebrook <typebrook@gmail.com>
2019-12-30 01:16:50 +0800

    tmp

--- rescue/script/track_group.js
+++ rescue/script/track_group.js
@@ -1,93 +1,29 @@
+var gpxFiles
+var layers
+fetch('http://api.github.com/repos/typebrook/ol/releases/tags/v1')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    gpxFiles = myJson.assets;
+  });
+for (var gpx in gpxFiles) {
+  layers.push(
+    new ol.layer.Vector({
+      title: 'foo',
+      visible: false,
+      source: new ol.source.Vector({
+        url: 'gpx.browser_download_url',
+        format: new ol.format.GPX()
+      }),
+      style: function (feature) {
+        // return style_update(feature);
+        return style_setter(feature, '#FFEC00')
+      }
+    }),
+  )
+}
 var track_group = new ol.layer.Group({
-    'title': 'GPX maps',
-    layers: [
-        new ol.layer.Vector({
-            title: '1215-1222馬望海搜救路線',
-            visible: false,
-            source: new ol.source.Vector({
-              url: './data/1215-1222馬望海搜救路線._Lite.gpx',
-              format: new ol.format.GPX()
-            }),
-            style: function(feature) {
-                // return style_update(feature);
-                return style_setter(feature,'#FFEC00')
-              }
-          }),
-
-        new ol.layer.Vector({
-          title: '20191223陳芃+天人_B區神木群',
-          source: new ol.source.Vector({
-            url: './data/20191223陳芃+天人_B區神木群.gpx',
-            format: new ol.format.GPX()
-          }),
-          style: function(feature) {
-              return style_setter(feature,'#00FFEC');
-
-            }
-          // projection: new ol.Projection("EPSG:4326")
-        }),
-
-
-        new ol.layer.Vector({
-          title: '2019-12-24 tracks',
-          source: new ol.source.Vector({
-            url: './data/1224_lite.gpx',
-            format: new ol.format.GPX()
-          }),
-          style: function(feature){
-            return style_setter(feature,'#FF000');
-          }
-          // projection: new ol.Projection("EPSG:4326")
-        }),
-
-
-      new ol.layer.Vector({
-        title: '2019-12-25 tracks',
-        source: new ol.source.Vector({
-          url:'./data/1225_lite.gpx',
-          format: new ol.format.GPX()
-        }),
-        style: function(feature){
-          return style_setter(feature,'#FF0000');
-        }
-      }),
-
-      new ol.layer.Vector({
-        title: '2019-12-26 tracks',
-        source: new ol.source.Vector({
-          url:'./data/1226_lite.gpx',
-          format: new ol.format.GPX()
-        }),
-        style: function(feature){
-          return style_setter(feature,'#008E29');
-        }
-      }),
-
-
-
-      new ol.layer.Vector({
-        title: '2019-12-27 tracks',
-        source: new ol.source.Vector({
-          url:'./data/1227_lite.gpx',
-          format: new ol.format.GPX()
-        }),
-        style: function(feature){
-          return style_setter(feature,'#1F95B2');
-        }
-      }),
-
-
-      new ol.layer.Vector({
-        title: '2019-12-28 tracks',
-        source: new ol.source.Vector({
-          url:'./data/1228_lite.gpx',
-          format: new ol.format.GPX()
-        }),
-        style: function(feature){
-          return style_setter(feature,'#D67500');
-        }
-      }),
-
-    ]
-
+  'title': 'GPX maps',
+  layers
 })
